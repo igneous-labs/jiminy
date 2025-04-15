5773be1422df12d345d037d36d5fa3c85644e6c1
billythedummy <dhy1996@live.com.sg>
2025-04-15 10:21:49 +0800

    cleanup return-data

--- return-data/src/lib.rs
+++ return-data/src/lib.rs
@@ -6,21 +6,15 @@ use core::mem::MaybeUninit;
 /// Maximum size that can be set using [`set_return_data`].
 pub const MAX_RETURN_DATA: usize = 1024;
 
-type ReturnDataLen = u16;
-
-const _ASSERT_RETURN_DATA_LEN_BITWIDTH_SUFFICIENT: () =
-    if (ReturnDataLen::MAX as usize) < MAX_RETURN_DATA {
-        panic!("ReturnDataLen type bitwidth insufficient for MAX_RETURN_DATA");
-    };
-
-/// `N` must be <= [`crate::MAX_RETURN_DATA`]
+/// `MAX_DATA_LEN` must be <= [`crate::MAX_RETURN_DATA`]
 #[derive(Debug, Clone, Copy)]
 pub struct ReturnData<const MAX_DATA_LEN: usize = MAX_RETURN_DATA> {
+    len: usize,
     program_id: MaybeUninit<[u8; 32]>,
-    len: ReturnDataLen,
     buf: [MaybeUninit<u8>; MAX_DATA_LEN],
 }
 
+// Accessors
 impl<const MAX_DATA_LEN: usize> ReturnData<MAX_DATA_LEN> {
     #[inline]
     pub const fn program_id(&self) -> &[u8; 32] {
@@ -33,24 +27,19 @@ impl<const MAX_DATA_LEN: usize> ReturnData<MAX_DATA_LEN> {
     }
 
     #[inline]
-    pub const fn data_len_raw(&self) -> ReturnDataLen {
+    pub const fn data_len(&self) -> usize {
         self.len
     }
 
     #[inline]
-    pub const fn data_len(&self) -> usize {
-        self.len as usize
-    }
-
-    #[inline]
     pub const fn is_empty(&self) -> bool {
-        self.data_len_raw() == 0
+        self.data_len() == 0
     }
 }
 
 #[inline]
 pub fn set_return_data(data: &[u8]) {
-    #[cfg(target_os = "solana")]
+    //#[cfg(target_os = "solana")]
     unsafe {
         jiminy_syscall::sol_set_return_data(data.as_ptr(), data.len() as u64);
     }
@@ -62,36 +51,51 @@ pub fn set_return_data(data: &[u8]) {
     }
 }
 
-/// `N` must be <= [`crate::MAX_RETURN_DATA`]
-#[inline]
-pub fn get_return_data<const MAX_DATA_LEN: usize>() -> Option<ReturnData<MAX_DATA_LEN>> {
-    #[cfg(target_os = "solana")]
-    {
-        const UNINIT: MaybeUninit<u8> = MaybeUninit::uninit();
-        let mut res: ReturnData<MAX_DATA_LEN> = ReturnData {
-            program_id: MaybeUninit::uninit(),
-            len: 0,
-            buf: [UNINIT; MAX_DATA_LEN],
-        };
-        let size = unsafe {
-            jiminy_syscall::sol_get_return_data(
-                res.buf.as_mut_ptr().cast(),
-                MAX_DATA_LEN as u64,
-                res.program_id.as_mut_ptr().cast(),
-            )
-        };
-        if size == 0 {
-            None
-        } else {
-            let size = core::cmp::min(size as usize, MAX_DATA_LEN);
-            // as-safety: MAX_DATA_LEN <= MAX_RETURN_DATA precondition
-            res.len = size as ReturnDataLen;
-            Some(res)
+impl<const MAX_DATA_LEN: usize> ReturnData<MAX_DATA_LEN> {
+    /// Return data copied via syscall is truncated to `MAX_DATA_LEN`.
+    ///
+    /// Returns `None` if syscall returned size 0 i.e. no program has set
+    /// return data so far or return data has been cleared.
+    #[inline]
+    pub fn get() -> Option<Self> {
+        #[cfg(target_os = "solana")]
+        {
+            const UNINIT: MaybeUninit<u8> = MaybeUninit::uninit();
+
+            let mut res = Self {
+                len: 0,
+                program_id: MaybeUninit::uninit(),
+                buf: [UNINIT; MAX_DATA_LEN],
+            };
+            let size = unsafe {
+                jiminy_syscall::sol_get_return_data(
+                    res.buf.as_mut_ptr().cast(),
+                    MAX_DATA_LEN as u64,
+                    res.program_id.as_mut_ptr().cast(),
+                )
+            };
+            if size == 0 {
+                None
+            } else {
+                // just being defensive here
+                let size = core::cmp::min(size as usize, MAX_DATA_LEN);
+                res.len = size;
+                Some(res)
+            }
+        }
+
+        #[cfg(not(target_os = "solana"))]
+        {
+            unreachable!()
         }
     }
+}
 
-    #[cfg(not(target_os = "solana"))]
-    {
-        unreachable!()
-    }
+/// For API symmetry with [`set_return_data`].
+///
+/// Uses [`ReturnData::get`] under the hood, and is probably more ergonomic
+/// to just use that since the const generics can be inferred in some contexts
+#[inline]
+pub fn get_return_data<const MAX_DATA_LEN: usize>() -> Option<ReturnData<MAX_DATA_LEN>> {
+    ReturnData::<MAX_DATA_LEN>::get()
 }
