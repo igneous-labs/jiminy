905937b7aec72643e859f90224d4a402bac739e8
billythedummy <dhy1996@live.com.sg>
2025-08-26 21:10:10 +0800

    rename

--- test-utils/src/lib.rs
+++ test-utils/src/lib.rs
@@ -44,8 +44,15 @@ pub fn two_different_pubkeys() -> impl Strategy<Value = [[u8; 32]; 2]> {
     })
 }
 
+const BENCH_RES_DIR: &str = "bench-res";
+
 pub fn save_cus_to_file(name: &str, compute_units_consumed: u64) {
-    let mut f = File::create(PathBuf::from("bench-cus").join(name).with_extension("txt")).unwrap();
+    let mut f = File::create(
+        PathBuf::from(BENCH_RES_DIR)
+            .join(name)
+            .with_extension("cus.txt"),
+    )
+    .unwrap();
     f.write_all(compute_units_consumed.to_string().as_bytes())
         .unwrap();
 }
