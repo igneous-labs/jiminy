e281b921bcebd67882b90322fcdeed78137c97fd
billythedummy <dhy1996@live.com.sg>
2025-10-24 16:56:20 +0800

    add append test

--- test-programs/README.md
+++ test-programs/README.md
@@ -7,3 +7,6 @@ Simple programs for testing the framework.
 ```sh
 cargo-test-sbf
 ```
+
+> [!WARNING]  
+> The programs in here do not care about security as they're meant to just test isolated components of jiminy. Deploying them to actual networks will result in loss of funds.
---
+++ test-programs/append/src/lib.rs
@@ -0,0 +1,34 @@
+//! This program reallocs then appends the received instruction data into an account owned by it
+//!
+//! Note that this program cannot work on any actual solana network, because there is no way for it
+//! to create the program owned account in the first place if it doesnt already exist.
+
+#![allow(unexpected_cfgs)]
+
+use jiminy_entrypoint::{
+    account::{Abr, AccountHandle},
+    program_error::{ProgramError, NOT_ENOUGH_ACCOUNT_KEYS},
+};
+
+pub const MAX_ACCS: usize = 1;
+
+jiminy_entrypoint::entrypoint!(process_ix, MAX_ACCS);
+
+fn process_ix(
+    abr: &mut Abr,
+    accounts: &[AccountHandle],
+    data: &[u8],
+    _prog_id: &[u8; 32],
+) -> Result<(), ProgramError> {
+    let [slab] = *accounts else {
+        return Err(NOT_ENOUGH_ACCOUNT_KEYS.into());
+    };
+
+    let slab = abr.get_mut(slab);
+    let old_len = slab.data_len();
+    slab.grow_by(data.len(), false)?;
+
+    slab.data_mut()[old_len..].copy_from_slice(data);
+
+    Ok(())
+}
